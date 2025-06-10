957be0ef286dd4ba4ff2b90dcb94ff50464d194d
Peyton <pmdavenport09@gmail.com>
2025-06-10 13:24:23 -0500

    Created most of home page: logo, two buttons, setting and streak emoji

---
+++ MediTrack1/MedicationInfo.swift
@@ -0,0 +1,18 @@
+//
+//  MedicationInfo.swift
+//  MediTrack1
+//
+//  Created by Scholar on 6/10/25.
+//
+
+import SwiftUI
+
+struct MedicationInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MedicationInfo()
+}
---
+++ MediTrack1/MedicationLog.swift
@@ -0,0 +1,18 @@
+//
+//  MedicationLog.swift
+//  MediTrack1
+//
+//  Created by Scholar on 6/10/25.
+//
+
+import SwiftUI
+
+struct MedicationLog: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MedicationLog()
+}
