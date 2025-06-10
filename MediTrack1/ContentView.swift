818c314b02a56da137af69ac6019f9a4004a164a
Peyton <pmdavenport09@gmail.com>
2025-06-10 10:09:02 -0500

    Initial Commit

---
+++ MediTrack1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MediTrack1
+//
+//  Created by Scholar on 6/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MediTrack1/MediTrack1App.swift
@@ -0,0 +1,17 @@
+//
+//  MediTrack1App.swift
+//  MediTrack1
+//
+//  Created by Scholar on 6/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MediTrack1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
